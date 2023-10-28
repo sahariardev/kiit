a604a26a926ba384400317cd39d30b4ab0fae1dc
sahariardev <rifatsahariar@gmail.com>
2023-10-28 13:35:25 +0600

    Added navbar

--- app/(home)/_components/footer.tsx
+++ app/(home)/_components/footer.tsx
@@ -1,11 +1,20 @@
 'use client'
 
 import {Logo} from "@/app/(home)/_components/logo";
+import {Button} from "@/components/ui/button";
 
 export const Footer = () => {
     return (
         <div className="flex items-center w-full p-6 bg-background z-50">
            <Logo/>
+            <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
+                  <Button variant="ghost" size="sm">
+                      Privacy Policy
+                  </Button>
+                  <Button variant="ghost" size="sm">
+                        Terms and Condition
+                  </Button>
+            </div>
         </div>
     );
 }
